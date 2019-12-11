4a7f8faf56c99effa5b29ffe014d634f8254a3fc
JoseManuel <JoseManuel@localhost>
2019-12-12 00:01:09 +0100

    kata 2 completo

--- SistemaRomano.js
+++ SistemaRomano.js
@@ -7,98 +7,228 @@ const romanValues = [1, 5, 10, 50, 100, 500, 1000];
 
 
 let romanToArabic = (roman) => {
+    let romanString = new String(roman).toUpperCase();
+    let romanStringLength = romanString.length;
+    let arabicDigit = 0;
+    let isSkippingLetter = false;
+
+    for (let i = 0; i < romanStringLength; i++) {
+
+        if (!isSkippingLetter) {
+            switch (romanString[i]) {
+                case "I":
+
+                    if (romanString[i + 1] === "V") {
+                        arabicDigit = arabicDigit + 4;
+                        isSkippingLetter = true;
+                    } else if (romanString[i + 1] === "X") {
+                        arabicDigit = arabicDigit + 9;
+                        isSkippingLetter = true;
+                    } else {
+                        arabicDigit = arabicDigit + 1;
+                    }
+                    break;
+                case "V":
+
+                    arabicDigit = arabicDigit + 5;
+
+                    break;
+
+                case "X":
+                    if (romanString[i + 1] === "L") {
+                        arabicDigit = arabicDigit + 40;
+                        isSkippingLetter = true;
+                    } else if (romanString[i + 1] === "C") {
+                        arabicDigit = arabicDigit + 90;
+                        isSkippingLetter = true;
+                    } else {
+                        arabicDigit = arabicDigit + 10;
+                    }
+
+                    break;
+
+                case "L":
+
+                    arabicDigit = arabicDigit + 50;
+
+                    break;
+
+                case "C":
+                    if (romanString[i + 1] === "D") {
+                        arabicDigit = arabicDigit + 400;
+                        isSkippingLetter = true;
+                    } else if (romanString[i + 1] === "M") {
+                        arabicDigit = arabicDigit + 900;
+                        isSkippingLetter = true;
+                    } else {
+                        arabicDigit = arabicDigit + 100;
+                    }
+                    break;
+                case "D":
+
+                    arabicDigit = arabicDigit + 500;
+
+                    break;
+                case "M":
+
+                    arabicDigit = arabicDigit + 1000;
+
+                    break;
+                default:
+                    return false;
+
+            }
+
+        } else {
+            isSkippingLetter = false;
+        }
+    }
+    return arabicDigit;
 
 }
 
 let arabicToRoman = (arabic) => {
-    let numArabic = Number(arabic);
-    if (numArabic > 3999 || numArabic < 1) return "Numero no covertible";
+    let arabicDigit = Number(arabic);
+    if (arabicDigit > 3999 || arabicDigit < 1) return "Numero no covertible";
     let romanString = "";
 
     for (let i = 6; i >= 0; i--) {
 
-        while (numArabic - romanValues[i] >= 0) {
-            numArabic = numArabic - romanValues[i];
+        while (arabicDigit - romanValues[i] >= 0) {
+            arabicDigit = arabicDigit - romanValues[i];
             romanString += romanLetters[i];
         }
 
         if (i > 0) {
             if (i % 2 === 0) {
                 let minValueToSub = (romanValues[i] - romanValues[i - 2]);
-                // console.log(minValueToSub);
-                if (numArabic >= minValueToSub && numArabic < romanValues[i]) {
-                    numArabic = numArabic - minValueToSub;
+                if (arabicDigit >= minValueToSub && arabicDigit < romanValues[i]) {
+                    arabicDigit = arabicDigit - minValueToSub;
                     romanString += romanLetters[i - 2] + romanLetters[i];
                 }
             } else {
                 let minValueToSub = (romanValues[i] - romanValues[i - 1]);
 
-                if (numArabic >= minValueToSub && numArabic < romanValues[i]) {
-                    numArabic = numArabic - minValueToSub;
+                if (arabicDigit >= minValueToSub && arabicDigit < romanValues[i]) {
+                    arabicDigit = arabicDigit - minValueToSub;
                     romanString += romanLetters[i - 1] + romanLetters[i];
                 }
             }
         }
     }
-
-    // while (numArabic - 1000 >= 0) {
-    //     numArabic = numArabic - 1000;
-    //     romanString += "M";
-    // }
-    // if (numArabic >= 900 && numArabic < 1000) {
-    //     numArabic = numArabic - 900;
-    //     romanString += "CM";
-    // }
-    // while (numArabic - 500 >= 0) {
-    //     numArabic = numArabic - 500;
-    //     romanString += "D";
-    // }
-    // if (numArabic >= 400 && numArabic < 500) {
-    //     numArabic = numArabic - 400;
-    //     romanString += "CD";
-    // }
-    // while (numArabic - 100 >= 0) {
-    //     numArabic = numArabic - 100;
-    //     romanString += "C";
-    // }
-    // if (numArabic >= 90 && numArabic < 100) {
-    //     numArabic = numArabic - 90;
-    //     romanString += "XC";
-    // }
-    // while (numArabic - 50 >= 0) {
-    //     numArabic = numArabic - 50;
-    //     romanString += "L";
-    // }
-    // if (numArabic >= 40 && numArabic < 50) {
-    //     numArabic = numArabic - 40;
-    //     romanString += "XL";
-    // }
-    // while (numArabic - 10 >= 0) {
-    //     numArabic = numArabic - 10;
-    //     romanString += "X";
-    // }
-    // if (numArabic >= 9 && numArabic < 10) {
-    //     numArabic = numArabic - 9;
-    //     romanString += "IX";
-    // }
-    // while (numArabic - 5 >= 0) {
-    //     numArabic = numArabic - 5;
-    //     romanString += "V";
-    // }
-    // if (numArabic >= 4 && numArabic < 5) {
-    //     numArabic = numArabic - 4;
-    //     romanString += "IV";
-    // }
-
-    // while (numArabic - 1 >= 0) {
-    //     numArabic = numArabic - 1;
-    //     romanString += "I";
-    // }
     return romanString;
-
 }
+
 let isRomanNumber = (roman) => {
 
+    let romanString = new String(roman).toUpperCase();
+    let romanStringLength = romanString.length;
+
+    for (let i = romanStringLength - 1; i >= 0; i--) {
+
+        switch (romanString[i]) {
+            case "I":
+
+                for (let j = i - 3; j >= 0; j--) {
+                    if (romanString[j] === "I")
+                        return false;
+                }
+                if (romanString[i - 1] === "I") {
+                    let higherRomanLetters = romanLetters.splice(1);
+                    if (higherRomanLetters.includes(romanString[i + 1])) {
+                        return false;
+                    }
+                }
+                break;
+            case "V":
+
+                for (let j = i - 1; j >= 0; j--) {
+                    if (romanString[j] === "V")
+                        return false;
+                }
+                for (let j = i + 1; j < romanStringLength; j++) {
+                    let higherRomanLetters = romanLetters.splice(2);
+                    if (higherRomanLetters.includes(romanString[j])) {
+                        return false;
+                    }
+                }
+                break;
+
+            case "X":
+                for (let j = i - 3; j >= 0; j--) {
+                    if (romanString[j] === "X")
+                        return false;
+                }
+
+                if (romanString[i - 1] === "X") {
+                    let higherRomanLetters = romanLetters.splice(3);
+                    if (higherRomanLetters.includes(romanString[i + 1])) {
+                        return false;
+                    }
+                }
+
+                break;
+
+            case "L":
+
+                for (let j = i - 1; j >= 0; j--) {
+                    if (romanString[j] === "L")
+                        return false;
+                }
+                for (let j = i + 1; j < romanStringLength; j++) {
+                    let higherRomanLetters = romanLetters.splice(4);
+                    if (higherRomanLetters.includes(romanString[j])) {
+                        return false;
+                    }
+                }
+                break;
+
+            case "C":
+                for (let j = i - 3; j >= 0; j--) {
+                    if (romanString[j] === "C")
+                        return false;
+                }
+
+                if (romanString[i - 1] === "C") {
+                    let higherRomanLetters = romanLetters.splice(5);
+                    if (higherRomanLetters.includes(romanString[i + 1])) {
+                        return false;
+                    }
+                }
+                break;
+            case "D":
+
+                for (let j = i - 1; j >= 0; j--) {
+                    if (romanString[j] === "D")
+                        return false;
+                }
+                for (let j = i + 1; j < romanStringLength; j++) {
+                    let higherRomanLetters = romanLetters.splice(6);
+                    if (higherRomanLetters.includes(romanString[j])) {
+                        return false;
+                    }
+                }
+                break;
+            case "M":
+
+                for (let j = i - 3; j >= 0; j--) {
+                    if (romanString[j])
+                        return false;
+                }
+
+                for (let j = i - 1; j >= 0; j--) {
+                    if (romanString[j] != "M" && romanString[j] != "C") {
+                        return false;
+                    }
+                }
+                break;
+            default:
+                return false;
+
+        }
+
+    }
+    return true;
 }
 
 
@@ -108,6 +238,60 @@ if (isNumber) {
     if (isRomanNumber(argument)) {
         console.log(romanToArabic(argument));
     } else {
-        console.log("Input no valido");
+        console.log("Input NO valido");
     }
 }
+
+    // while (arabicDigit - 1000 >= 0) {
+    //     arabicDigit = arabicDigit - 1000;
+    //     romanString += "M";
+    // }
+    // if (arabicDigit >= 900 && arabicDigit < 1000) {
+    //     arabicDigit = arabicDigit - 900;
+    //     romanString += "CM";
+    // }
+    // while (arabicDigit - 500 >= 0) {
+    //     arabicDigit = arabicDigit - 500;
+    //     romanString += "D";
+    // }
+    // if (arabicDigit >= 400 && arabicDigit < 500) {
+    //     arabicDigit = arabicDigit - 400;
+    //     romanString += "CD";
+    // }
+    // while (arabicDigit - 100 >= 0) {
+    //     arabicDigit = arabicDigit - 100;
+    //     romanString += "C";
+    // }
+    // if (arabicDigit >= 90 && arabicDigit < 100) {
+    //     arabicDigit = arabicDigit - 90;
+    //     romanString += "XC";
+    // }
+    // while (arabicDigit - 50 >= 0) {
+    //     arabicDigit = arabicDigit - 50;
+    //     romanString += "L";
+    // }
+    // if (arabicDigit >= 40 && arabicDigit < 50) {
+    //     arabicDigit = arabicDigit - 40;
+    //     romanString += "XL";
+    // }
+    // while (arabicDigit - 10 >= 0) {
+    //     arabicDigit = arabicDigit - 10;
+    //     romanString += "X";
+    // }
+    // if (arabicDigit >= 9 && arabicDigit < 10) {
+    //     arabicDigit = arabicDigit - 9;
+    //     romanString += "IX";
+    // }
+    // while (arabicDigit - 5 >= 0) {
+    //     arabicDigit = arabicDigit - 5;
+    //     romanString += "V";
+    // }
+    // if (arabicDigit >= 4 && arabicDigit < 5) {
+    //     arabicDigit = arabicDigit - 4;
+    //     romanString += "IV";
+    // }
+
+    // while (arabicDigit - 1 >= 0) {
+    //     arabicDigit = arabicDigit - 1;
+    //     romanString += "I";
+    // }
